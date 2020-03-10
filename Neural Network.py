60ca69707a0259b6dc764531fe36dcfa8d8b9760
RaiyaanAbdullah <raiyaanabdullah@gmail.com>
2020-03-11 02:25:53 +0600

    Update Neural Network.py

--- Neural Network.py
+++ Neural Network.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 import numpy as np
 
-(x_train, y_train) = np.load('train_data.npy') 
-(x_test, y_test) = np.load('test_data.npy')
+train_data = np.load('train_data.npy') 
+test_data = np.load('test_data.npy')  
+(x_train, y_train) = (train_data[0],train_data[1])
+(x_test, y_test) = (test_data[0],test_data[1])
 
-
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
 
 print('Training data shape: ', x_train.shape)
 print('Testing data shape : ', x_test.shape)
