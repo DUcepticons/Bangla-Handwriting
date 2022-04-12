8378fc5961be5394a2a2448a23de2e0e86394b60
Raiyaan Abdullah <raiyaanabdullah@gmail.com>
2022-04-12 20:35:45 +0600

    data split

--- Datasets/Landmark detection/supervisely_to_coco_annotation.py
+++ Datasets/Landmark detection/supervisely_to_coco_annotation.py
@@ -33,10 +33,17 @@ for i in range(0,25):
     keypoints_dict[joint_names[i]]={"X":keypoints[i*3],"Y":keypoints[i*3+1]}
 '''
 
-output = {}
+train_output = {}
+train_images = []
+train_annotations = []
 
-images = []
-annotations = []
+val_output = {}
+val_images = []
+val_annotations = []
+
+test_output = {}
+test_images = []
+test_annotations = []
 
 for idx, json_file in enumerate(os.listdir(json_files_location)):
     file_path = os.path.join(json_files_location,json_file)
@@ -47,7 +54,13 @@ for idx, json_file in enumerate(os.listdir(json_files_location)):
     image["height"] = data['size']['height']
     image["width"] = data['size']['width']
     image["id"] = idx
-    images.append(image)
+    
+    if idx <= 940:
+        train_images.append(image)
+    elif idx >940 and idx <=990:
+        val_images.append(image)
+    else:
+        test_images.append(image)
     
     annotation = {}
     annotation["image_id"] = idx
@@ -65,15 +78,31 @@ for idx, json_file in enumerate(os.listdir(json_files_location)):
         keypoints[number*3] = sup_keypoint["points"]["exterior"][0][0]
         keypoints[number*3+1] = sup_keypoint["points"]["exterior"][0][1]
         keypoints[number*3+2] = 2
+        
+
+        
     annotation["keypoints"] = keypoints
-    
-    annotations.append(annotation)
+
+    if idx <= 940:
+        train_annotations.append(annotation)
+    elif idx >940 and idx <=990:
+        val_annotations.append(annotation)
+    else:
+        test_annotations.append(annotation)
         
-        
-        
-    
     
 
-output["images"]= images 
-output["annotations"]= annotations    
+train_output["images"]= train_images 
+train_output["annotations"]= train_annotations    
+with open("train_annotations.json", "w") as write_file:
+    json.dump(train_output, write_file, indent=4)
     
+val_output["images"]= val_images 
+val_output["annotations"]= val_annotations    
+with open("val_annotations.json", "w") as write_file:
+    json.dump(val_output, write_file, indent=4)
+    
+test_output["images"]= test_images 
+test_output["annotations"]= test_annotations    
+with open("test_annotations.json", "w") as write_file:
+    json.dump(test_output, write_file, indent=4)
