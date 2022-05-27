30ba60d1bbbccbecdcb6d5f76d98ab5a382c6618
Raiyaan Abdullah <raiyaanabdullah@gmail.com>
2022-05-27 13:01:42 +0600

    new data

--- Datasets/Landmark detection/rename.py
+++ Datasets/Landmark detection/rename.py
@@ -1,9 +1,9 @@
 import os
 
-dir = "D:\Github Projects\Bangla-Handwriting\Datasets\Landmark detection\\a-augment-1000\\a\\"
+dir = "D:\Github Projects\Bangla-Handwriting\Datasets\Landmark detection\\c\\c\\"
 
 for count, filename in enumerate(os.listdir(dir)):
-        dst ='a-'+str(count+1) + ".jpg"
+        dst ='c-'+str(count+1) + ".jpg"
         #dst ="flipped-"+ filename[:-8] + "good.mp4"
         src =dir+ filename
         dst =dir+ dst
